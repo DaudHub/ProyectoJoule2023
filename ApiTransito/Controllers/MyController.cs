7014007801f27e084f955a5b278932428fbadc08
DaudHub <mateodaud@gmail.com>
2023-08-01 17:02:52 -0300

    metodos de verificacion diferentes para usuarios y camioneros

--- ApiTransito/Controllers/MyController.cs
+++ ApiTransito/Controllers/MyController.cs
@@ -11,7 +11,8 @@ namespace ApiAlmacenes.Controllers;
 public class MyController : Controller {
     
     public MySqlConnection db_conn = new ("Server=127.0.0.1;User ID=apialmacen;Password=urbgieubgiutg98rtygtgiurnindg8958y;Database=proyecto");
-    private bool VerifyCredentials(Verification ver) {
+    
+    private bool VerifyCredentialsForTruckDriver(Verification ver) {
         try {
             if (db_conn.State == ConnectionState.Closed) 
                 db_conn.Open();
@@ -23,7 +24,29 @@ public class MyController : Controller {
             var reader = command.ExecuteReader();
             if (!reader.HasRows) return false;
             while (reader.Read()) {
-                if (reader.GetString(1) != "almacenero" && reader.GetString(1) != "admin") return false;
+                if (reader.GetString(1) != "camionero" && reader.GetString(1) != "admin") return false;
+            }
+            reader.Close();
+            return true;
+        }
+        catch(Exception) {
+            throw;
+        }
+    }
+
+    private bool VerifyCredentialsForUser(Verification ver) {
+        try {
+            if (db_conn.State == ConnectionState.Closed) 
+                db_conn.Open();
+            MySqlCommand command = new (null, db_conn);
+            command.CommandText = 
+                @$"select proyecto.usuarios.usuario, rol 
+                from proyecto.usuarios inner join proyecto.tokens on proyecto.usuarios.usuario=proyecto.tokens.usuario 
+                where token='{ver.Token}' and pwd='{MyEncryption.EncryptToString(ver.Password)}'";
+            var reader = command.ExecuteReader();
+            if (!reader.HasRows) return false;
+            while (reader.Read()) {
+                if (reader.GetString(1) != "usuario" && reader.GetString(1) != "camionero" && reader.GetString(1) != "admin") return false;
             }
             reader.Close();
             return true;
